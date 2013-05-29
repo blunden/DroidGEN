1532518d29a94e990a3c06dea3dc9e91f8407904
blunden <blunden2@gmail.com>
2013-05-29 13:27:44 +0200

    Restore Main.java

---
+++ src/com/droidhits/genesisdroid/Main.java
@@ -0,0 +1,14 @@
+package com.droidhits.genesisdroid;
+
+import android.os.Bundle;
+
+import com.droidhits.genesisdroid.MainActivity;
+
+public class Main extends MainActivity {
+
+	public void onCreate(Bundle savedInstanceState)
+    {
+		
+		super.onCreate(savedInstanceState);
+    }
+}
